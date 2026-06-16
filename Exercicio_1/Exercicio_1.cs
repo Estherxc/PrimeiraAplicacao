112c2eed11d4ac4cd72850020d02ae40ec4dafa1
Esther C. de Sousa <esther.correa@aluno.educa.go.gov.br>
2026-06-16 14:36:53 +0000

    Exercicio_1

---
+++ Exercicio_1/Exercicio_1.cs
@@ -0,0 +1,13 @@
+﻿{
+    {
+        {
+            string nome = "Esther";
+            
+            string sobrenome = "Sousa";
+            
+        
+           Console.WriteLine("Olá, " + nome + " " + sobrenome + " Seja bem-vindo à aula de Programação de Aplicativos.");
+           
+        }
+    }
+}
